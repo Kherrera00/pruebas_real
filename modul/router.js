a9012d7087ab2c4525d0b54bb07d26ddfd86d251
Kherrera00 <kherrera174@gmail.com>
2022-10-22 15:31:50 -0500

    a2

---
+++ modul/router.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+
+const conexion = require('./database/db');
+
+
+router.get('/', (req,res) => {
+  conexion.query('SELECT * FROM usuario', (error,results) =>{
+    if(error){
+      throw error;
+    }else {
+      res.send(results);
+    }
+  });
+
+});
+
+module.exports = router;
